b724c4444224e09d65224fb144cd5871bcd38be5
jsy3356 <jy339@njit.edu>
2019-12-16 16:18:36 +0000

    final commit

--- hypebeasts/forms.py
+++ hypebeasts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class HyperForm(forms.Form):
-    product_name = forms.CharField(label='Product name', max_length=100)
+    product_name = forms.CharField(label='Product Name', max_length=100)
     amount = forms.IntegerField()
